936df179a2e749e35ad80543658c6a2f0c82cc35
Marcin Polski <marcin.polski98@gmail.com>
2018-01-09 13:06:30 +0100

    wip

--- server/server.js
+++ server/server.js
@@ -4,13 +4,20 @@ import pg from 'pg';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const dbUrl = process.env.DATABASE_URL || 'postgres://jxpiolxvcavudw:5ca7d5239b2360e639e09e791ebc934ddc4698825fdbebe0e638bd93ee280ce9@ec2-54-83-59-144.compute-1.amazonaws.com:5432/dd3u2ejnlqqah4';
 
-/* app.post('/movies', (req, res) => {
+app.post('/movies', (req, res) => {
 
 });
 
 app.get('/movies', (req, res) => {
+  pg.connect(dbUrl, (err, client, done) => {
+    client.query('SELECT * FROM test_table', (err, result) => {
+      if (err) return res.send(`Error: ${err}`);
 
+      res.send(result);
+    });
+  });
 });
 
 app.post('/comments', (req, res) => {
@@ -19,19 +26,6 @@ app.post('/comments', (req, res) => {
 
 app.get('/comments', (req, res) => {
 
-}); */
-
-app.get('/', (req, res) => {
-  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
-    client.query('SELECT * FROM test_table', (err, result) => {
-      done();
-      if (err) {
-        return res.send(`Error: ${err}`);
-      }
-
-      return res.send(result);
-    });
-  });
 });
 
 app.listen(port, () => {
