54122c21ee164765d6260eba3ad4d3953c0836d1
Marcin Polski <marcin.polski98@gmail.com>
2018-01-09 11:45:21 +0100

    wip

--- server/server.js
+++ server/server.js
@@ -21,16 +21,8 @@ app.get('/comments', (req, res) => {
 
 }); */
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result) }
-    });
-  });
+app.get('/', (req, res) => {
+  res.send('This is my site');
 });
 
 app.listen(port, () => {
